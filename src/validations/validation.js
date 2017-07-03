c12a5633a466241b66f8c6fe394ef304574d9246
Tom Gehrke <phyrog@cognitive-coding.com>
2017-07-03 11:19:45 +0200

    Improve debouncing

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -1,36 +1,33 @@
-import debounce from "debounce-promise";
-
-/**
- * further validators:
- * - value
- * - email
- */
-
 class Validation {
   constructor(options, debounced) {
     this.options = options;
     this.id = Symbol();
     if (debounced) {
-      this.run = debounce(this.run.bind(this), debounced);
+      this.debounce = fn => setTimeout(fn, debounced);
     }
+  }
+  debounce(fn) {
+    fn();
   }
   cancel() {}
   run(value, values) {
     this.cancel();
     return new Promise((resolve, reject) => {
       this.cancel = reject;
-      Promise.resolve(this.test(value, values)).then(
-        result => {
-          if (result) {
-            resolve();
-          } else {
+      this.debounce(() => {
+        Promise.resolve(this.test(value, values)).then(
+          result => {
+            if (result) {
+              resolve();
+            } else {
+              reject(this.failureMessage(value, values));
+            }
+          },
+          () => {
             reject(this.failureMessage(value, values));
           }
-        },
-        () => {
-          reject(this.failureMessage(value, values));
-        }
-      );
+        );
+      });
     });
   }
 }
